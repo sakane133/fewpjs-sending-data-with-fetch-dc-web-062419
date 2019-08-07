d4757b27b6e5d8def367e34ca2b126adfb6dce09
Samantha Kane <s.kane133@gmail.com>
2019-08-07 15:25:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Add your code here
 function submitData(name, email){
   return 
-fetch("http://localhost:3000/dogs", configObj)
-
-  
-  
-  
-  
-  
+fetch("http://localhost:3000/dogs", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  })
+})
 }
