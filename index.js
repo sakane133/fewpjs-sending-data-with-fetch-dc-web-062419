ad7db397ed8421470572b485b0b5e3fde968f07c
Samantha Kane <s.kane133@gmail.com>
2019-08-07 15:28:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ function submitData(name, email){
   return 
 fetch("http://localhost:3000/users", {
   method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
 })
 }
 
+    
